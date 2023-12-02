8d6862e4fd5aa9f355c273ae6134c07411d38899
DeibyKwl <75148109+DeibyKwl@users.noreply.github.com>
2023-12-02 16:32:58 -0500

    Create checking_env.py

---
+++ checking_env.py
@@ -0,0 +1,13 @@
+from game_cus_gym_env import *
+
+env = Game_env()
+episodes = 50
+for episode in range(episodes):
+    obs = env.reset()
+    while True:
+        env.render()
+        random_action = env.action_space.sample()
+        obs,reward,truncated,done,info = env.step(random_action)
+        if env.done: 
+            print('reward',reward)
+            break
