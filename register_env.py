4aaa580b707ee3423ea8ac56442d3733168af6f4
DeibyKwl <deibykwl@gmail.com>
2023-12-10 11:58:05 -0500

    Update register_env.py

--- register_env.py
+++ register_env.py
@@ -4,6 +4,13 @@ def register_custom_env():
     gym.envs.register(
         id='dodge_game_env-v0',
         entry_point='game_cus_gym_env:Game_env',  
-        max_episode_steps=100,  # Optional: Define maximum episode steps
-        reward_threshold=10,   # Optional: Set the reward threshold
+        max_episode_steps=10000,  # Optional: Define maximum episode steps
+        reward_threshold=1000,   # Optional: Set the reward threshold
+    )
+
+    gym.envs.register(
+        id='dodge_game_env-v1',
+        entry_point='game_cus_gym_env_version_2:Game_env',  
+        max_episode_steps=10000,  # Optional: Define maximum episode steps
+        reward_threshold=1000,   # Optional: Set the reward threshold
     )
